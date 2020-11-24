ca6422b31a8911dc7ab439f2655f4d0d8004d12b
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-11-24 09:14:48 +0200

    ül1.php lahendus

---
+++ ül1.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    Ülesanne 1
+    Karl-August Kongo
+    24.11.2020
+ */
+$nimi = 'Karl-August Kongo';
+$synniaasta = 1999;
+$tahtkuju = 'lõvi';
+
+echo 'Minu nimi on '.$nimi.'!<br>';
+echo 'Minu sünniaasta on '.$synniaasta.'.<br>';
+echo 'Minu tähtkuju on '.$tahtkuju.'.<br>';
+echo "\"It's My Life\" - Dr. Alban<br>";
+
+echo'(\(\'<br>';
+echo'(-.-)'.'<br>';
+echo'o_(")(")';
+
