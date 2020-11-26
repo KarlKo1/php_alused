f9ff0dfc24b39829e861b9e060cd9fd595769e0a
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-11-26 10:38:24 +0200

    yl2.php lahendus

--- yl2.php
+++ yl2.php
@@ -14,9 +14,15 @@ $jaak = $x %= $y;
 echo "Vastused:<br> liitmine 6 + 2 = $liitm,<br> lahutamine 6 - 2 = $lahut,<br> korrutamine 6 * 2 = $korru,<br> jagamine 6 / 2 = $jagam,<br> jääk 6 % 2 = $jaak";
 
 
-$i = 123456;
-$sent = $i * 100;
-$meet = $i * 1000;
-$vormindatud = sprintf('<br> Vastus sentimeetriteks: $sent %0.2f', $i);
-$vormindatud2 = sprintf('<br> Vastus meetriteks: $meet %0.2f', $i);
-echo $vormindatud, $vormindatud2;
+$i = 123000;
+printf('<br>Vastus: %0.3f', $i / 10);
+printf('<br>Vastus: %0.3f', $i / 1000);
+
+
+$a = 4;
+$b = 5;
+$c = 6.4;
+$kolmnurga_p = $a + $b + $c;
+$kolmnurga_s = ($a * $b) / 2;
+
+printf("<br>Kolmnurga ümbermõõt on ".$kolmnurga_p." ja kolmnurga pindala on ".$kolmnurga_s.".");
