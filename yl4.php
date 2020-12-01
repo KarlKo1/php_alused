83f7ef3d3ee20658b96b0b697c9c59eca4aeee68
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-12-01 08:46:27 +0200

    yl4.php ja yl4.html lahendus

--- yl4.php
+++ yl4.php
@@ -9,12 +9,14 @@ $arv1 = $_POST['arv1'];
 $arv2 = $_POST['arv2'];
 $jagamine = $arv1 / $arv2;
 
-if($arv1 == 0){
+if(empty($arv1)) {
+    echo 'Lahter on tühi';
+}elseif($arv1 == 0){
     echo 'Nulliga ei tohi jagada!';
 } elseif ($arv2 == 0){
     echo 'Nulliga ei tohi jagada!';
 } else {
-    echo $jagamine;
+    echo 'Jagamise vastus: '.$jagamine;
 }
 
 
