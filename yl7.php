6ce0a47975a6583a3d5ee904cbfdc7ebbf3a5e65
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-12-09 11:24:38 +0200

    yl7 lahendused

---
+++ yl7.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    Ülesanne 3
+    Karl-August Kongo
+    26.11.2020
+ */
+
+//Tervitus
+function tervita(){
+    return "Tere päiksekesekene!";
+}
+
+echo tervita();
+
+//Liitu uudiskirjaga
