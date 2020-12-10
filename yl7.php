f2e6a182fe01afcdb96469964307b18b7b83912f
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-12-10 11:45:29 +0200

    yl7.php ja yl7.html lahendus

--- yl7.php
+++ yl7.php
@@ -16,20 +16,70 @@ echo tervita();
 
 
 //Kasutajanimi ja email
-$nimi = $_POST['nimi'];
-function ($nimi) {
-    $nimii = (strtolower($nimi));
-    echo $nimii;
+/*
+    $var = strtolower($_POST['nimi']);
+    echo $var.'@hkhk.edu.ee';
+*/
+/*
+function nimii($_POST)
+{
+    return strtolower($_POST['nimi']);
 }
+echo nimii($_POST).'@hkhk.edu.ee';
+*/
+
+
 
 //Arvud
+/*
+function nimii($_POST)
+{
+    return range($_POST['nimi']);
+}
+echo nimii($_POST);
+*/
 
 
 
-//Ristküliku pindala
+/*Ristküliku pindala
+$alus = $_POST['alus'];
+$korgus = $_POST['korgus'];
+function pindala($alus, $korgus) {
+    echo 'Pindala on '.$alus*$korgus.' ühikut.<br>';
+}
+echo pindala($alus, $korgus);
+*/
 
 
-//Isikukood
+/*Isikukood
+$isik = $_POST['isik'];
+function isikukood($isik)
+{
+    if (strlen($isik) == 11) {
+        echo "Isikukood on õige!";
+    } else {
+        echo "Isikukood on vale!";
+    } if (substr($isik, 1, 1) == 3) {
+        echo "<br> Tegemist on meessoost isikuga.";
+}   else {
+        echo "<br> Tegemist on naissoost isikuga.";
+}
+}
+echo isikukood($isik);
+*/
 
 
 //Head mõtted
+function mote()
+{
+    $alus = array('Koer', 'Kass', 'Mees', 'Naine');
+    $oeldis = array('kõnnib', 'jookseb', 'hüppab', 'vaatab');
+    $sihitis = array('poodi', 'laevale', 'kiiresti', 'aeglasti');
+
+    $rand1 = rand(1, 4);
+    $rand2 = rand(1, 4);
+    $rand3 = rand(1, 4);
+
+    echo $alus[$rand1] . " " . $oeldis[$rand2] . " " . $sihitis[$rand3] . ".";
+}
+echo mote();
