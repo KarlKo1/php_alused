2534959cc270b77bc44b09417069d7b67ba238e8
Karl-August Kongo <karl-august.kongo@khk.ee>
2021-01-05 14:11:17 +0200

    config

--- PHP ja MySQL/config.php
+++ PHP ja MySQL/config.php
@@ -9,6 +9,6 @@ $db_salasona = '';
 $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 //ühenduse kontroll
 if(!$yhendus){
-    die('Ei saa ühendust andmebaasiga');
+    die('Ei saa ühendust andmebaasiga!');
 }
 ?>
